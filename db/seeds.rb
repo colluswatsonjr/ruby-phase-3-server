49df44f4b1522e12667b0a0d6c3bbbb07963e23d
Collus Watson <cklwatsonjr@gmail.com>
2022-08-22 20:51:54 -0400

    'create seeds'

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,16 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+Nation.create(nation_name:'Hyrule', nation_location:'Eastern Mountains')
+Nation.create(nation_name:'Earth Kingdom', nation_location:'Canyons')
+Nation.create(nation_name:'Whiterun', nation_location:'Valley')
+
+Role.create(role_title:'King', role_rating:"S")
+Role.create(role_title:'Knight', role_rating:"B")
+Role.create(role_title:'Servant',  role_rating:'F')
+
+Character.create(character_name:'Lj', nation_id:Nation.all.ids.sample, role_id:Role.all.ids.sample)
+Character.create(character_name:'Jr', nation_id:Nation.all.ids.sample, role_id:Role.all.ids.sample)
+Character.create(character_name:'Tre', nation_id:Nation.all.ids.sample, role_id:Role.all.ids.sample)
 
 puts "✅ Done seeding!"
