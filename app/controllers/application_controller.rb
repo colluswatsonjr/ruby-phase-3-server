db329f0c5be4a307d73bc90d392869e0f2f5f02b
Collus Watson <cklwatsonjr@gmail.com>
2022-08-22 21:55:20 -0400

    'add get requests to retreive database data'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,17 @@ class ApplicationController < Sinatra::Base
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
+  # Character --------
+  get "/characters" do
+    Character.all.to_json
+  end
+  # Nation --------
+  get "/nations" do
+    Nation.all.to_json
+  end
+  # Role --------
+  get "/roles" do
+    Role.all.to_json
+  end
 
 end
