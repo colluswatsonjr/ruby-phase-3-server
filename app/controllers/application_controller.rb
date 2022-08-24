f5ffae097ce11ae5dc5123556ce3d106e4dd3f9e
Collus Watson <cklwatsonjr@gmail.com>
2022-08-23 20:18:05 -0400

    'craate character post, patch, delete requests'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,35 @@ class ApplicationController < Sinatra::Base
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
+
   # Character --------
+
   get "/characters" do
     Character.all.to_json
   end
+
+  get "/characters/:id" do
+    character = Character.find(params[:id])
+    character.to_json
+  end
+
+  post "/characters" do
+    character = Character.create(character_name: params[:character_name], nation_id: params[:nation_id], role_id: params[:role_id])
+    character.to_json
+  end
+
+  patch "/characters/:id" do
+    character = Character.find(params[:id])
+    character.update(character_name: params[:character_name], nation_id: params[:nation_id], role_id: params[:role_id])
+    character.to_json
+  end
+
+  delete "/characters" do
+    character = Character.find(params[:id])
+    character.destroy
+    character.to_json
+  end
+
   # Nation --------
   get "/nations" do
     Nation.all.to_json
